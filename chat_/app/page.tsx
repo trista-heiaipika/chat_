ab293688307c2975829b76bfc4af4ecc93ca0f18
trista-heiaipika <huangxin.gan@motiong.ai>
2025-02-19 10:14:12 +0800

    Update page.tsx

--- chat_/app/page.tsx
+++ chat_/app/page.tsx
@@ -5,15 +5,15 @@ import RightPanel from '@/components/RightPanel';
 export default function Home() {
   return (
     <div className="flex h-screen bg-background">
-      {/* 左侧边栏 */}
+      {/* left sidebar */}
       <Sidebar className="w-64 border-r border-gray-200 dark:border-gray-800" />
       
-      {/* 中间聊天区域 */}
+      {/* middle chat area */}
       <main className="flex-1 flex flex-col">
         <ChatArea />
       </main>
 
-      {/* 右侧面板 */}
+      {/* right panel */}
       <RightPanel className="w-64 border-l border-gray-200 dark:border-gray-800" />
     </div>
   );
